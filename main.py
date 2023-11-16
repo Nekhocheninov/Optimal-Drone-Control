ca3136fc9cefeca22fee98c6d7a4666fd314db30
Dmitry <128161876+Nekhocheninov@users.noreply.github.com>
2023-11-16 20:26:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ l     = 2.8956  # размах  крыла в метрах
 m     = 13.5    # масса самолета при полных баках в килограммах
 e     = 0.75    # коэффициент Освальда
 g     = 9.80665 # ускорение свободного падения
-u     = 10      # угол тангажа
-o     = 0       # траекторный угол
-m_c   = 0.0002  # секундный расход топлива при максимальной тяге в килограммах в секунду
-P_max = 70      # максимальное значение тяги двигателя
+u     = np.radians(10) # угол тангажа     в градусах
+o     = np.radians(10) # траекторный угол в градусах
+m_c   = 0.0002  # секундный расход топлива при максимальной тяге в кг в секунду
+P_max = 70      # максимальное значение тяги двигателя в Ньютонах
 V_t0  = 20      # начальная скорость в метрах в секунду
-V_T   = 20      # конечная  скорость в метрах в секунду
+V_T   = 50      # конечная  скорость в метрах в секунду
 H_t0  = 50      # текущая   высота   в метрах
 H_T   = 1000    # конечная  высота   в метрах
 
@@ -24,11 +24,14 @@ def air_density(height):
     T0 = 288.15    # стандартная температура на уровне моря в Кельвинах
     P0 = 101325    # стандартное давление    на уровне моря в Паскалях
     L  = 0.0065    # градиент температуры в Кельвинах на метр
-    R  = 8.31447   # универсальная газовая постоянная в Дж/(моль·К)
+    R  = 287.05    # универсальная газовая постоянная в Дж/(кг·К)
     M  = 0.0289644 # молярная масса воздуха в кг/моль
-    T = T0 - L * height                                   # Рассчет температуры на заданной высоте
-    P = P0 * (1 - L * height / T0) ** (9.8 * M / (R * L)) # Рассчет давления    на заданной высоте
-    rho = P / (R * T)                                     # Рассчет плотности по уравнению состояния газа
+    # Рассчет температуры на заданной высоте
+    # Рассчет давления    на заданной высоте
+    # Рассчет плотности по уравнению состояния газа
+    T = T0 - L * height
+    P = P0 * (1 - L * height / T0) ** (9.8 * M / (R * L))
+    rho = P / (R * T)
     return rho
 
 # Функция управления тягой при наборе высоты
@@ -40,29 +43,34 @@ def control_a(H, V, P):
     return (m * g - c_y0 * S * air_density(H) * V**2 / 2) / (c_ya * S * air_density(H) * V**2 / 2 + P)
 
 # Функции, представляющие систему дифференциальных уравнений при наборе высоты
-def system_H(t, y):
-    o, H, m, V = y
+def system_H(t, y, V):
+    o, H, m = y
     do = t * ((control_P(H, V) * (u - o) + (c_y0 + c_ya * (u - o)) * S * air_density(H) * V**2 / 2) / (m * V) - (g * np.cos(o) / V))
     dH = t * V * np.sin(o)
     dm = t * (-m_c)
-    return [do, dH, dm, V]
+    return [do, dH, dm]
 
 # Функции, представляющие систему дифференциальных уравнений при наборе скорости
-def system_V(t, y):
-    V, x, m, H = y
+def system_V(t, y, H):
+    V, x, m = y
     dV = t * (P_max - (c_x0 + (c_ya * control_a(H, V, P_max)**2 * S) / (np.pi * e * l**2) * S * air_density(H) * V**2 / 2)) / m
     dx = t * V
     dm = t * (-m_c)
-    return [dV, dx, dm, H]
+    return [dV, dx, dm]
 
-t_span = (0, 5) # Время
-initial_conditions_H = [o, H_t0, m, V_t0]
+time_values = np.arange(0, 10.1, 0.1)
+t_span = (0, 10) # Время
 
-solution_H = solve_ivp(system_H, t_span, initial_conditions_H, t_eval=np.linspace(0, 5, 5))
-
-print(system_H(1, initial_conditions_H))
-print(solution_H.y[1])
+initial_conditions_V = [V_t0, 0, m]
+solution_H = solve_ivp(lambda t, y: system_V(t, y, H_t0), t_span, initial_conditions_V, t_eval = time_values)
 plt.plot(solution_H.t, solution_H.y[0], label='o(t)')
 plt.plot(solution_H.t, solution_H.y[1], label='H(t)')
 plt.plot(solution_H.t, solution_H.y[2], label='m(t)')
+plt.show()
+
+initial_conditions_H = [o, H_t0, m]
+solution_V = solve_ivp(lambda t, y: system_H(t, y, V_t0), t_span, initial_conditions_H, t_eval = time_values)
+plt.plot(solution_V.t, solution_V.y[0], label='V(t)')
+plt.plot(solution_V.t, solution_V.y[1], label='x(t)')
+plt.plot(solution_V.t, solution_V.y[2], label='m(t)')
 plt.show()
